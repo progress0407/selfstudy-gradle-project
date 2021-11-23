ffc98181d0ebc526779c801a8375fc1590ab5f09
swcho <progress0407@gmail.com>
2021-11-24 08:52:45 +0900

    refactor(InputViewStation): LINE_SEPARATOR 개행 라인 수정

--- src/main/java/nextsteptdd/subwaymap/view/InputViewStation.java
+++ src/main/java/nextsteptdd/subwaymap/view/InputViewStation.java
@@ -3,17 +3,11 @@ package nextsteptdd.subwaymap.view;
 public class InputViewStation extends InputView {
 
     private static final String SHOW_DISPLAY =
-            "## 역 관리 화면" +
-                    LINE_SEPARATOR +
-                    "1. 역 등록" +
-                    LINE_SEPARATOR +
-                    "2. 역 삭제" +
-                    LINE_SEPARATOR +
-                    "3. 역 조회" +
-                    LINE_SEPARATOR +
-                    "B. 돌아가기" +
-                    LINE_SEPARATOR +
-                    LINE_SEPARATOR +
+            "## 역 관리 화면" + LINE_SEPARATOR +
+                    "1. 역 등록" + LINE_SEPARATOR +
+                    "2. 역 삭제" + LINE_SEPARATOR +
+                    "3. 역 조회" + LINE_SEPARATOR +
+                    "B. 돌아가기" + LINE_SEPARATOR + LINE_SEPARATOR +
                     "## 원하는 기능을 선택하세요.";
 
     public OutputView inputNumber() {
