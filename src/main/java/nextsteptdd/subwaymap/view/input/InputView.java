2673d40a8aa71c3ffe3691ae8fa6915b6a37e76b
swcho <progress0407@gmail.com>
2021-11-26 00:05:17 +0900

    refactor: throw new 에서 콘솔 출력으로 변경

--- src/main/java/nextsteptdd/subwaymap/view/input/InputView.java
+++ src/main/java/nextsteptdd/subwaymap/view/input/InputView.java
@@ -9,7 +9,7 @@ import static java.lang.System.out;
 public abstract class InputView {
 
     protected static final String LINE_SEPARATOR = System.lineSeparator();
-    private static final String ERROR_OUT_OF_MENU = "선택할 수 없는 기능입니다.";
+    private static final String ERROR_OUT_OF_MENU = "[ERROR] 선택할 수 없는 기능입니다.";
     protected static Scanner scanner = new Scanner(System.in);
 
     protected static void showMenu(final String SHOW_DISPLAY) {
@@ -22,11 +22,11 @@ public abstract class InputView {
         int anInt = 0;
         try {
             anInt = Integer.parseInt(input);
+            if (anInt < 1 || anInt > 4) {
+                out.println(ERROR_OUT_OF_MENU);
+            }
         } catch (Exception e) {
-            throw new IllegalArgumentException(ERROR_OUT_OF_MENU);
-        }
-        if (anInt < 1 || anInt > 4) {
-            throw new IllegalArgumentException(ERROR_OUT_OF_MENU);
+            out.println(ERROR_OUT_OF_MENU);
         }
     }
 }
